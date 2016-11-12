3709ed43a635da84a434d20b72c90e1ec2b2d445
Ivey Topaz <iveytopaz@gmail.com>
2016-11-12 16:38:48 -0500

    testing DB connection

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
+const mysql = require('mysql');
 
 const port = process.env.PORT || 49151;
-
 const ip = process.env.IP || '127.0.0.1';
 app = express();
+
+
+const connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
+
+//Testing DB connection
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
 
 app.use(morgan('dev'))
 app.use(express.static('TLdist'))
