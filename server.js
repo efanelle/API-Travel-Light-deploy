f9031698beff07bfea0936e06d045530517c009a
Ivey Topaz <iveytopaz@gmail.com>
2016-11-12 14:47:20 -0500

    adding morgan

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const port = process.env.PORT || 49151;
 
 const ip = process.env.IP || '127.0.0.1';
 app = express();
 
+app.use(morgan('dev'))
 app.use(express.static('TLdist'))
 
-app.listen(process.env.PORT, function() {
-  console.log('The magic is on ' + ip + ':' + process.env.PORT);
+app.listen(port, function() {
+  console.log('The magic is on ' + ip + ':' + port);
 });
