7217a55f7c5007323f4ee83f160babd74edeba01
Erica Fanelle <efanelle@gmail.com>
2016-11-14 17:37:43 -0500

    Added plane and car emission/cost data

---
+++ data/planes.js
@@ -0,0 +1,15 @@
+
+// average emissions are in lbs (per hour and mile)
+// these numbers are per passenger
+const planeEmissions = {
+  perHour: 330,
+  perMile: 0.63
+};
+
+const planeSpeed = 528;
+//miles per hour
+
+module.exports = {
+  planeEmissions: planeEmissions,
+  planeSpeed:planeSpeed
+}
