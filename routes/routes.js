66604d5ea72a36e05986f49c6aa1a6d0274dc308
Erica Fanelle <efanelle@gmail.com>
2016-11-14 19:33:10 -0500

    Info for flight duration/co2 emissions added

--- routes/routes.js
+++ routes/routes.js
@@ -137,6 +137,8 @@ module.exports = function(app) {
   //   }, (err, response, body) => {
   //     console.log(response)
   //   })
+
+
       let pointA = [39, -104]
       let pointB = [33, -84]
       let planeDist = distance.caclulateDist(pointA, pointB); //DEN to ATL
@@ -160,45 +162,12 @@ module.exports = function(app) {
           return duration;
         }
 
-<<<<<<< HEAD
-let pointA = [39, -104]
-      let pointB = [33, -84]
-      let planeDist = distance.caclulateDist(pointA, pointB); //DEN to ATL
-      let duration = '';
-      function time(dist){
-        let time= dist/planeInfo.planeSpeed;
-        time = time * 60 + 30 //added 30 min to account for slower speeds during takeoff and landing
-        var hour = 0, min, duration;
-          while(time > 60) {
-            time -= 60;
-            hour+=1;
-            min = Math.round(time);
-          }
-          if (hour > 1) {
-            duration = hour + ' hours, ' + min + ' min';
-          } else if( hour === 1) {
-            duration = hour + ' hour, ' + min + ' min';
-          } else {
-            duration = min + ' min';
-          }
-          return duration;
-        }
-
-=======
->>>>>>> Info for flight duration/co2 emissions added
       let planeStats = {
         duration: time(planeDist), //hrs and minutes
         emissions: Math.round(planeDist * planeInfo.planeEmissions.perMile*100)/100
         //lbs of CO2
       }
       console.log(planeStats);
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> Info for flight duration/co2 emissions added
 
     let options = {
       url: `http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/${market}/${currency}/${locale}/${origin}/${destination}/${outboundDate}/${inboundDate}?apiKey=${process.env.SKYSCANNER_API_KEY}`
