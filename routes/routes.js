be4fe8f17891376536e384635b59ca39e2c8ac3e
Ivey Topaz <iveytopaz@gmail.com>
2016-11-15 02:49:16 -0500

    adding live pricing data

--- routes/routes.js
+++ routes/routes.js
@@ -3,26 +3,28 @@ const Promise = require('bluebird');
 const distance = require('../helpers/distance');
 const db = require('../db/connection');
 const request = require('request');
+const rp = require('request-promise');
 const jStat = require('jStat').jStat;
-const cheerio = require('cheerio');
 const planeInfo = require('../data/planes');
 const carInfo = require('../data/cars');
-
+const timeCalc = require('../helpers/time')
 
 
 
 module.exports = function(app) {
-  //Testing price crawler
-  app.get('/test', (req, res) => {
-    // request('https://www.kayak.com/flights/ATL-SFO/2016-11-25', (err, response, body) => {
-    //   const $ = cheerio.load(body)
-    //   let timeText = $('div.duration').text()
-    //    console.log(timeText)
-    //   res.status(200).send({time: timeText, body: $('html > body').text()})
-    // })
-    `https://api.flightstats.com/flex/schedules/${protocol}/v1/${format}/${...}`
-  })
 
+//retrieving all US airports
+  app.get('/api/airports', (req, res) => {
+    db.Airports.findAll({
+      where: {
+        Country: 'United States'
+      },
+      raw: true
+    })
+    .then((airports) => {
+      res.status(200).send(airports)
+    })
+  })
 
   // Retreive car distance and location data
   app.get('/api/cars', (req,res) => {
@@ -84,36 +86,117 @@ module.exports = function(app) {
     });
   });
 
-//retrieving all US airports
-  app.get('/api/airports', (req, res) => {
-    db.Airports.findAll({
-      where: {
-        Country: 'United States'
-      },
-      raw: true
-    })
-    .then((airports) => {
-      res.status(200).send(airports)
-    })
-  })
 
   app.get('/api/planes', (req, res) => {
-    // Should recieve origin airport, destination, with lat / lng, and date of travel
 
-  // Calculate distance btwn 2 coordinates
-  // ie. distance.caclulateDist([34, 84], [35, 85])
-
-    // Get flight price from skyscanner
-    // Can give user a direct flights only option
     let market = 'US';
     let currency = 'USD';
     let locale = 'en-US';
-    let origin = 'ATL';
-    let destination = 'DEN';
-    let outboundDate = '2016-12-10';
-    let inboundDate = '2016-12-14';
+    let origin = 'JFK';
+    let destination = 'SFO';
+    let outboundDate = '2016-12-26';
     let locationSchema = 'Iata';
     let passengers = 1;
+    let year = outboundDate.slice(0, 4);
+    let month = outboundDate.slice(5, 7);
+    let day = outboundDate.slice(8, 10);
+    let pointA = [39, -104]
+    let pointB = [33, -84]
+    let planeDist = distance.caclulateDist(pointA, pointB); //DEN to ATL
+
+    let planeStats = {
+      emissions: Math.round(planeDist * planeInfo.planeEmissions.perMile*100)/100
+      //lbs of CO2
+    }
+
+    // url for flight time
+    let url = `https://api.flightstats.com/flex/schedules/rest/v1/json/from/${origin}/to/${destination}/departing/${year}/${month}/${day}?appId=${process.env.FLIGHTSTATS_API_ID}&appKey=${process.env.FLIGHTSTATS_API_KEY}`;
+    // url for flight price
+    let options = {
+      url: `http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/${market}/${currency}/${locale}/${origin}/${destination}/${outboundDate}/?apiKey=${process.env.SKYSCANNER_API_KEY}`
+    }
+    var arrivalTZAdjust;
+    var departureTZAdjust;
+    const p1 = rp(url)
+      .then(body => {
+        return new Promise ((resolve, reject) => {
+          body = JSON.parse(body);
+          db.Airports.findOne({
+            where: {
+              FAA_IATA: destination
+            },
+            raw: true
+          })
+          .then(airport1 => {
+            arrivalTZAdjust = airport1.Timezone
+            db.Airports.findOne({
+            where: {
+              FAA_IATA: origin
+            },
+            raw: true
+            })
+            .then(airport2 => {
+              // calculate time of flight, adjusting for timezone difference
+                departureTZAdjust = airport2.Timezone
+                let times = body.scheduledFlights.map(flight => {
+                  let arrival = new Date(flight.arrivalTime);
+                  let departure = new Date(flight.departureTime);
+                  let utc1 = Date.UTC(arrival.getFullYear(), arrival.getMonth(), arrival.getDate(), arrival.getHours(), arrival.getMinutes())
+                  let utc2 = Date.UTC(departure.getFullYear(), departure.getMonth(), departure.getDate(), departure.getHours(), departure.getMinutes())
+                  let adjust = arrivalTZAdjust - departureTZAdjust;
+                  return (utc1 - utc2) / 1000 / 60 / 60 - adjust
+                })
+                // add .5 to account for runway time
+                let avgTime = jStat.mean(times) + .5
+                resolve({time: avgTime, timeString: timeCalc(avgTime)})
+            })
+          })
+        })
+      })
+    const p2 = rp(options)
+      .then(body => {
+        return new Promise ((resolve, reject) => {
+          body = JSON.parse(body)
+          if (body.Quotes.length === 0) {
+            outboundDate = outboundDate.slice(0, -3);
+            let options = {
+              url: `http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/${market}/${currency}/${locale}/${origin}/${destination}/${outboundDate}/?apiKey=${process.env.SKYSCANNER_API_KEY}`
+            }
+            request(options, (err, response, body) => {
+              if (err) {
+                console.log(err)
+              }
+              body = JSON.parse(body)
+              // Get array of prices
+              let priceArray = body.Quotes.map(quote => quote.MinPrice);
+              // Generate statistics object
+              let jStatPrices = jStat(priceArray);
+              // Find mean and standard deviation
+              let meanPrice = jStatPrices.mean();
+              let range = jStatPrices.stdev()
+              // Filter to prices within 2.5 stdevs of the mean
+              let filteredPrices = priceArray.filter(price => {
+                return price < meanPrice + range * 2.5 && price > meanPrice - range * 2.5
+              })
+              // Generate stats object from filtered prices
+              let filteredJStatPrices = jStat(filteredPrices);
+              let minPrice = filteredJStatPrices.min()
+              resolve({price: minPrice, type: 'average', emissons: planeStats.emissions})
+            })
+          } else {
+            let minPrice = body.Quotes[0].MinPrice
+            resolve({price: minPrice, type: 'single day', emissons: planeStats.emissions})
+          }
+        })
+      })
+    Promise.all([p1, p2])
+    .then(results => {
+      const finalObj = Object.assign({}, results[0], results[1])
+      res.status(200).send(finalObj)
+    })
+  })
+}
+
 
   // Open session to populate prices
   // Need live flight data access to use this method
@@ -140,88 +223,3 @@ module.exports = function(app) {
   //   }, (err, response, body) => {
   //     console.log(response)
   //   })
-
-
-      let pointA = [39, -104]
-      let pointB = [33, -84]
-      let planeDist = distance.caclulateDist(pointA, pointB); //DEN to ATL
-      let time= planeDist/planeInfo.planeSpeed;
-
-      function timeCalc(time){
-        var minutes = time * 60 + 30 //added 30 min to account for slower speeds during takeoff and landing
-        var hour = 0, min, duration;
-          while(minutes > 60) {
-            minutes -= 60;
-            hour+=1;
-            min = Math.round(minutes);
-          }
-          if (hour > 1) {
-            duration = hour + ' hours, ' + min + ' min';
-          } else if( hour === 1) {
-            duration = hour + ' hour, ' + min + ' min';
-          } else {
-            duration = min + ' min';
-          }
-          return duration;
-        }
-
-      let planeStats = {
-        duration: timeCalc(time), //hrs and minutes
-        time: time,
-        emissions: Math.round(planeDist * planeInfo.planeEmissions.perMile*100)/100
-        //lbs of CO2
-      }
-      console.log(planeStats);
-
-    let options = {
-      url: `http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/${market}/${currency}/${locale}/${origin}/${destination}/${outboundDate}/${inboundDate}?apiKey=${process.env.SKYSCANNER_API_KEY}`
-    }
-    request(options, (err, response, body) => {
-      if (err) {
-        console.log(err)
-      }
-      body = JSON.parse(body)
-      if (body.Quotes.length === 0) {
-        outboundDate = outboundDate.slice(0, -3);
-        inboundDate = inboundDate.slice(0, -3);
-        let options = {
-          url: `http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/${market}/${currency}/${locale}/${origin}/${destination}/${outboundDate}/${inboundDate}?apiKey=${process.env.SKYSCANNER_API_KEY}`
-        }
-        request(options, (err, response, body) => {
-          if (err) {
-            console.log(err)
-          }
-          // Get array of prices
-          let priceArray = body.Quotes
-            .map(quote => quote.MinPrice);
-          // Generate statistics object
-          let jStatPrices = jStat(priceArray);
-          // Find mean and standard deviation
-          let meanPrice = jStatPrices.mean();
-          let range = jStatPrices.stdev()
-          // Filter to prices within 2.5 stdevs of the mean
-          let filteredPrices = priceArray.filter(price => {
-            return price < meanPrice + range * 2.5 && price > meanPrice - range * 2.5
-          })
-          // Generate stats object from filtered prices
-          let filteredJStatPrices = jStat(filteredPrices);
-          const priceObj = {
-            low: filteredJStatPrices.min(),
-            med: filteredJStatPrices.mean(),
-            high: filteredJStatPrices.max()
-          }
-          res.status(200).send({price: priceObj.low, type: 'average'})
-        })
-      } else {
-        let minPrice = body.Quotes[0].MinPrice
-        console.log(minPrice)
-        res.status(200).send(body)
-        //res.status(200).send({price: minPrice, type: 'single day'})
-        // res.status(200).send({price: minPrice, type: 'single day', duration: planeStats.duration, emissons: planeStats.emissions})
-      }
-    })
-
-  })
-
-}
-
