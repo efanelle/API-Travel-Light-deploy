1961e8a308fdf8e3a4543d5ff14b024cbb81b42d
PHDorin <hanshirosan@gmail.com>
2016-11-22 22:27:34 -0700

    fix API key issues

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -5,6 +5,7 @@ const timeCalc = require('../helpers/time')
 
 const getCarCosts = (req,res) => {
   let {airOriginLat, airOriginLng, airDestLat, airDestLng, driveOriginLat, driveOriginLng, driveDestLat, driveDestLng} = req.params;
+
   let api_key = process.env.GoogleMaps_API_KEY;
   console.log('api key ' + api_key)
   if (airOriginLat !== 'undefined') {
@@ -80,6 +81,37 @@ const getCarCosts = (req,res) => {
       // using static data for cost per mile
       const costPerMile = (data.carCosts.small + data.carCosts.med + data.carCosts.large)/3;
       const carCost = distance * costPerMile;
+
+  let api_key = process.env.GoogeMaps_API_KEY;
+  let airOrigin = [Number(airOriginLat), Number(airOriginLng)];
+  let airDestination = [Number(airDestLat), Number(airDestLng)];
+  let driveOrigin = [Number(driveOriginLat), Number(driveOriginLng)];
+  let driveDestination = [Number(driveDestLat), Number(driveDestLng)];
+  const options = {
+    url:`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${driveOrigin}|${airDestination}&destinations=${airOrigin}|${driveDestination}&key=${api_key}`
+  };
+  request(options, (err, response, body) => {
+    if (err) {
+      console.log(err);
+    }
+    body = JSON.parse(body)
+    //parsing data for distance output in meters, converted to miles
+    const MetersPerMile = 1609.34;
+    let distance = body.rows[0].elements[1].distance.value/MetersPerMile;
+    let carAirDistance = body.rows[0].elements[0].distance.value/MetersPerMile + body.rows[1].elements[1].distance.value/MetersPerMile;
+    //parsing data for time output in seconds, converted to hours
+    const SecondsPerHour = 3600;
+    let carTime = body.rows[0].elements[1].duration.value / SecondsPerHour;
+    let carTimeText = body.rows[0].elements[1].duration.text;
+    let carAirTime = body.rows[0].elements[0].duration.value / SecondsPerHour + body.rows[1].elements[1].duration.value/SecondsPerHour;
+    let carAirTimeText = timeCalc(carAirTime);
+    // using static data for emissions
+    const carEmissions = ((data.carEmissions.small + data.carEmissions.med + data.carEmissions.large) / 3)*distance;
+    const carAirEmissions = ((data.carEmissions.small + data.carEmissions.med + data.carEmissions.large) / 3)*carAirDistance;
+    // using static data for cost per mile
+    const costPerMile = (data.carCosts.small + data.carCosts.med + data.carCosts.large)/3;
+    const carCost = distance * costPerMile;
+    const carAirCost = carAirDistance * costPerMile
 
       const responseObj = {
         car: {
--- routes/routes.js
+++ routes/routes.js
@@ -10,13 +10,14 @@ module.exports = function(app) {
   //retrieving all US airports
   app.get('/api/airports', airports.getAllAirports)
 
-  app.get('/api/normalizers/:travelers/:originLat/:originLng/:destLat/:destLng',
-  averages.getAveragesByTripLength)
+  app.get('/api/normalizers/:travelers/:originLat/:originLng/:destLat/:destLng', averages.getAveragesByTripLength)
+  
   // Retreive car distance and location data
-  app.get('/api/cars/:airOriginLat/:airOriginLng/:airDestLat/:airDestLng/:driveOriginLat/:driveOriginLng/:driveDestLat/:driveDestLng',
-  cars.getCarCosts);
+  app.get('/api/cars/:airOriginLat/:airOriginLng/:airDestLat/:airDestLng/:driveOriginLat/:driveOriginLng/:driveDestLat/:driveDestLng', cars.getCarCosts);
+
   //Retrieve Transit costs
   app.get('/api/transit/:driveOriginLat/:driveOriginLng/:driveDestLat/:driveDestLng',transit.getTransitCosts)
+
   // Retreive car distance and location data
 
   app.get('/api/planes/:origin/:destination/:date/:travelers/:originLat/:originLng/:destLat/:destLng',
