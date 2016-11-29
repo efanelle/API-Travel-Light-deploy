5924f85a3be20cbebd3d6cadaa972c148c7ac179
Ivey Topaz <iveytopaz@gmail.com>
2016-11-28 20:22:05 -0500

    Provide average data for local routes

--- data/averages.js
+++ data/averages.js
@@ -1,6 +1,12 @@
 // Average for cars and planes, for comparison purposes
-module.exports = {
+exports.distant = {
   costCalc: (dist, travelers = 1) => .15 / 2 + (50 + .15 * dist) * travelers / dist / 2,
   emissions: 1.63/2,
   timeCalc: (dist) => (1.5 / 2 + (30 + 60 * dist / 528) / dist / 2) / 60
+}
+// time is in hours per mile
+exports.local = {
+  costCalc: (travelers) => (.204 * travelers + .14) / 3,
+  emissions: 1.34 / 3,
+  time: (1 / 3.1 + 1 / 25 + 1 / 10) / 3
 }
