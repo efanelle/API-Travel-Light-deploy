47275f0b987ad36f8ee3a43edd5e863cfe7c5348
Ivey Topaz <iveytopaz@gmail.com>
2016-11-28 20:23:06 -0500

    Add route for walking

--- routes/routes.js
+++ routes/routes.js
@@ -4,20 +4,25 @@ const averages = require('./averagesRoutes');
 const cars = require('./carRoutes');
 const planes = require('./planeRoutes');
 const transit = require('./transitRoutes');
-const trains = require('./trainRoutes')
+const trains = require('./trainRoutes');
+const walking = require('./walkingRoutes');
 
 module.exports = function(app) {
 
   //retrieving all US airports
   app.get('/api/airports', airports.getAllAirports)
 
-  app.get('/api/normalizers/:travelers/:originLat/:originLng/:destLat/:destLng', averages.getAveragesByTripLength)
+  // Averages for data display purposes
+  app.get('/api/normalizers/:travelers/:originLat/:originLng/:destLat/:destLng/:tripType', averages.getAveragesByTripLength)
 
   // Retreive car distance and location data
   app.get('/api/cars/:airOriginLat/:airOriginLng/:airDestLat/:airDestLng/:driveOriginLat/:driveOriginLng/:driveDestLat/:driveDestLng', cars.getCarCosts);
 
   //Retrieve Transit costs
-  app.get('/api/transit/:driveOriginLat/:driveOriginLng/:driveDestLat/:driveDestLng',/*transit.getTransitCosts*/(req, res) => res.status(200).send({}))
+  app.get('/api/transit/:driveOriginLat/:driveOriginLng/:driveDestLat/:driveDestLng/:travelers', transit.getTransitCosts)
+
+  //Retrieve Walking costs
+  app.get('/api/walking/:driveOriginLat/:driveOriginLng/:driveDestLat/:driveDestLng', walking.getWalkingCosts)
 
   // Retreive car distance and location data
   app.get('/api/planes/:origin/:destination/:date/:travelers/:originLat/:originLng/:destLat/:destLng',
